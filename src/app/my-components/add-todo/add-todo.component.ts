f36f7cf6ccf63fd5fde44a84235b17b138709cb0
afrid268 <79365878+afrid268@users.noreply.github.com>
2023-10-24 14:32:49 +0530

    first-commit
    completed todo functionality

---
+++ src/app/my-components/add-todo/add-todo.component.ts
@@ -0,0 +1,32 @@
+import { Component , OnInit ,Output , EventEmitter } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-add-todo',
+  templateUrl: './add-todo.component.html',
+  styleUrls: ['./add-todo.component.css']
+})
+export class AddTodoComponent implements OnInit{
+
+  todoTitle!: string;
+  todoDesc!:string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter();
+
+constructor() {}
+
+ngOnInit(): void {
+  
+}
+
+onSubmit(){
+  const todo = {
+    sno : 0,
+    title : this.todoTitle,
+    desc : this.todoDesc,
+    active : true
+  }
+  
+ this.todoAdd.emit(todo)
+  
+}
+}
